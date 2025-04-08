dab5bc3d720b42b611ea86c03ed1ebdb3f975e64
강병용 <byeongyong.kang@vuno.co>
2025-04-08 17:24:47 +0900

    fix: kakao custom message

--- src/entities/team/lib/share-kakao.ts
+++ src/entities/team/lib/share-kakao.ts
@@ -2,16 +2,13 @@ import type { ShareKakaoContent } from '@/entities'
 
 export const shareKakao = (content: ShareKakaoContent) => {
   const kakao = (window as unknown as { Kakao: any }).Kakao
-  const baseUrl = window.location.origin + window.location.pathname
-  const teamsParam = content.teams ? `?teams=${JSON.stringify(content.teams)}` : ''
-  const shareUrl = baseUrl + teamsParam
+  const teamsParam = content.teams ? `teams=${JSON.stringify(content.teams)}` : ''
 
-  kakao.Share.sendDefault({
-    objectType: 'text',
-    text: content.description,
-    link: {
-      webUrl: shareUrl,
-      mobileWebUrl: shareUrl
+  kakao.Share.sendCustom({
+    templateId: 119479,
+    templateArgs: {
+      description: content.description,
+      link: teamsParam
     }
   })
 }
