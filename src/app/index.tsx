c45161079c8dc3280d0dbb0ed81b2bd20c3f3b60
강병용 <byeongyong.kang@vuno.co>
2025-04-08 16:18:17 +0900

    fix: remove router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,26 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router'
 import { Provider } from './Provider'
-import { Home } from './ui'
 
 import '@ant-design/v5-patch-for-react-19'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: Home
-  },
-  {
-    path: '/urbancentro-fc',
-    Component: Provider
-  }
-])
 
 const root = document.getElementById('root')
 
 createRoot(root!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider />
   </StrictMode>
 )
